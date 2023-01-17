ece628d0f8b3ccc51c179aa5de976ab83945dc75
JustD <pr3dko.igor@gmail.com>
2023-01-16 20:31:39 -0500

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-function qs(selector, all = false) {
-  return all
-    ? document.querySelectorAll(selector)
-    : document.querySelector(selector);
-}
+const sections = document.querySelectorAll(".section");
+const timeline = document.querySelector(".timeline");
+const line = document.querySelector(".line");
 
-const sections = qs(".section", true);
-const timeline = qs(".timeline");
-const line = qs(".line");
 line.style.bottom = `calc(100% - 20px)`;
 let prevScrollY = window.scrollY;
 let up, down;
@@ -19,10 +14,8 @@ function scrollHandler(e) {
   up = scrollY < prevScrollY;
   down = !up;
   const timelineRect = timeline.getBoundingClientRect();
-  const lineRect = line.getBoundingClientRect(); // const lineHeight = lineRect.bottom - lineRect.top;
 
   const dist = targetY - timelineRect.top;
-  console.log(dist);
 
   if (down && !full) {
     set = Math.max(set, dist);
@@ -50,3 +43,155 @@ function scrollHandler(e) {
 scrollHandler();
 line.style.display = "block";
 window.addEventListener("scroll", scrollHandler);
+
+/////////////To the top///////////////////////
+
+const toTheTop = document.querySelector(".to-the-top");
+
+document.addEventListener("scroll", () => {
+  btnVisibility();
+});
+
+const btnVisibility = () => {
+  if (window.scrollY > 1000) {
+    toTheTop.style.display = "block";
+  } else {
+    toTheTop.style.display = "none";
+  }
+};
+
+toTheTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+//////////////Background //////////////////
+
+const back = document.querySelector(".background");
+
+const img1 = (back.style.background =
+  "url('img/back/ReBack5.jpg') no-repeat center");
+const img2 = (back.style.background =
+  "url('img/back/ReBack3.jpg') no-repeat center");
+const img3 = (back.style.background =
+  "url('img/back/ReBack2.jpg') no-repeat center");
+const img4 = (back.style.background =
+  "url('img/back/ReBack4.jpg') no-repeat center");
+
+let i = 0;
+function change() {
+  let images = [img1, img2, img3, img4];
+  back.style.background = images[i];
+  i = (i + 1) % images.length;
+  back.style.backgroundSize = "cover";
+}
+setInterval(change, 12000);
+
+////////////////modal/////////////////
+
+const modal = document.querySelectorAll(".modal");
+const content = document.querySelectorAll(".content");
+const mute = document.querySelectorAll(".soundIcon");
+const modalClose = document.querySelectorAll(".close");
+
+const res0 = document.querySelector(".res0");
+const res1 = document.querySelector(".res1");
+const res2 = document.querySelector(".res2");
+const res3 = document.querySelector(".res3");
+const rescv = document.querySelector(".rescv");
+const res4 = document.querySelector(".res4");
+const res5 = document.querySelector(".res5");
+const res6 = document.querySelector(".res6");
+const res7 = document.querySelector(".res7");
+const res8 = document.querySelector(".res8");
+
+let reAudio = new Audio();
+
+function reSound() {
+  content.forEach((el) => {
+    el.addEventListener("click", () => {
+      mute.forEach((m) => {
+        m.classList.remove("sound-icon-off");
+      });
+    });
+
+    el.addEventListener("click", () => {
+      if (el.classList.contains("re0-content")) {
+        reAudio.src = "sound/RE0-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        res0.classList.toggle("show");
+      } else if (el.classList.contains("re1-content")) {
+        reAudio.src = "sound/RE1-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        res1.classList.toggle("show");
+      } else if (el.classList.contains("re2-content")) {
+        reAudio.src = "sound/RE2-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        res2.classList.toggle("show");
+      } else if (el.classList.contains("re3-content")) {
+        reAudio.src = "sound/RE3-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        res3.classList.toggle("show");
+      } else if (el.classList.contains("recv-content")) {
+        reAudio.src = "sound/RECV-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        rescv.classList.toggle("show");
+      } else if (el.classList.contains("re4-content")) {
+        reAudio.src = "sound/RE4-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        res4.classList.toggle("show");
+      } else if (el.classList.contains("re5-content")) {
+        reAudio.src = "sound/RE5-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        res5.classList.toggle("show");
+      } else if (el.classList.contains("re6-content")) {
+        reAudio.src = "sound/RE6-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        res6.classList.toggle("show");
+      } else if (el.classList.contains("re7-content")) {
+        reAudio.src = "sound/RE7-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        res7.classList.toggle("show");
+      } else if (el.classList.contains("re8-content")) {
+        reAudio.src = "sound/RE8-SAVEROOM.mp3";
+        reAudio.autoplay = true;
+        res8.classList.toggle("show");
+      }
+    });
+  });
+}
+reSound();
+
+function soundMute() {
+  mute.forEach((m) => {
+    m.addEventListener("click", () => {
+      m.classList.toggle("sound-icon-off");
+      if (reAudio.paused) {
+        reAudio.play();
+      } else {
+        reAudio.pause();
+      }
+    });
+  });
+}
+soundMute();
+
+back.addEventListener("click", function (e) {
+  if (e.target !== modal) {
+    modal.forEach((e) => {
+      e.classList.remove("show");
+    });
+    reAudio.pause();
+  }
+});
+
+modalClose.forEach((e) => {
+  e.addEventListener("click", () => {
+    modal.forEach((m) => {
+      m.classList.remove("show");
+      reAudio.pause();
+    });
+  });
+});
